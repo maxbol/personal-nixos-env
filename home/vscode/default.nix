7126d8d9b123eeedf5f3d66a5ef5d60030c1571e
Max Bolotin <maks.bolotin@gmail.com>
2024-02-13 16:49:05 +0100

    vscode extensions work

--- home/vscode/default.nix
+++ home/vscode/default.nix
@@ -95,6 +95,7 @@ in
     package = (pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: rec {
       src = (builtins.fetchTarball {
         url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
+        sha256 = "";
       });
       version = "latest";
       buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 ];
