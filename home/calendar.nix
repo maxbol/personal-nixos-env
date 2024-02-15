779a37be658b6f5bb71d5a270b5abbe18b0a2791
Max Bolotin <maks.bolotin@gmail.com>
2024-02-15 17:41:13 +0100

    add sha256 to khal override

--- home/calendar.nix
+++ home/calendar.nix
@@ -6,6 +6,7 @@ in
   home.packages = with pkgs; [
     (import (builtins.fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz";
+      sha256 = "sha256:1l2hq1n1jl2l64fdcpq3jrfphaz10sd1cpsax3xdya0xgsncgcsi";
     }) {}).khal
     khard
     vdirsyncer
