44116827e5750b067fd4e8063ec66b0cfa3e6461
Max Bolotin <maks.bolotin@gmail.com>
2024-02-15 17:35:13 +0100

    attempt att kahl dependency fix

--- home/calendar.nix
+++ home/calendar.nix
@@ -4,7 +4,9 @@ let
 in
 {
   home.packages = with pkgs; [
-    khal
+    (import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz";
+    }) {}).khal
     khard
     vdirsyncer
   ];
