60d6ba563c61556cbb0917dfd7139def9c2dfa7f
Max Bolotin <maks.bolotin@gmail.com>
2024-02-15 16:44:41 +0100

    fix: minor typo

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -2,7 +2,7 @@
 , config
 , ...
 }: let
-  waybar-tooltip-khal = fetchurl "https://github.com/TheChymera/waybar-tooltips/blob/fb2a638fe58959f9f496d6e77396f746118e761a/bin/waybar-tooltip-khal.py";
+  waybar-tooltip-khal = builtins.fetchurl "https://github.com/TheChymera/waybar-tooltips/blob/fb2a638fe58959f9f496d6e77396f746118e761a/bin/waybar-tooltip-khal.py";
 in {
   home.file.".config/waybar/config".source = ./config.json;
   home.file.".config/waybar/style.css".source = ./style.css;
