9fed65395bf009a3268a00450e342868b0b9e368
Max Bolotin <maks.bolotin@gmail.com>
2024-02-15 17:02:22 +0100

    xdg associations cleanup

--- home/xdg.nix
+++ home/xdg.nix
@@ -4,29 +4,29 @@ let
 
   # XDG MIME types
   associations = {
+    "application/json" = browser;
+    "application/pdf" = [ "org.pwmt.zathura.desktop.desktop" ];
     "application/x-extension-htm" = browser;
     "application/x-extension-html" = browser;
     "application/x-extension-shtml" = browser;
     "application/x-extension-xht" = browser;
     "application/x-extension-xhtml" = browser;
     "application/xhtml+xml" = browser;
+    "audio/*" = [ "mpv.desktop" ];
+    "image/*" = [ "imv.desktop" ];
     "text/html" = browser;
+    "video/*" = [ "mpv.dekstop" ];
     "x-scheme-handler/about" = browser;
     "x-scheme-handler/chrome" = [ "chrome.desktop" ];
+    "x-scheme-handler/discord" = [ "discordcanary.desktop" ];
     "x-scheme-handler/ftp" = browser;
     "x-scheme-handler/http" = browser;
     "x-scheme-handler/https" = browser;
-    "x-scheme-handler/unknown" = browser;
-    "x-scheme-handler/webcal" = browser;
-
-    "audio/*" = [ "mpv.desktop" ];
-    "video/*" = [ "mpv.dekstop" ];
-    "image/*" = [ "imv.desktop" ];
-    "application/json" = browser;
-    "application/pdf" = [ "org.pwmt.zathura.desktop.desktop" ];
-    "x-scheme-handler/discord" = [ "discordcanary.desktop" ];
     "x-scheme-handler/spotify" = [ "spotify.desktop" ];
     "x-scheme-handler/tg" = [ "telegramdesktop.desktop" ];
+    "x-scheme-handler/unknown" = browser;
+    "x-scheme-handler/webcal" = browser;
+    "x-scheme-handler/notion" = "notion-app-enhanced.desktop";
   };
 in
 {
